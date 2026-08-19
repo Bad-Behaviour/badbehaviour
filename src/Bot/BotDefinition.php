1bfedd506828f8d8bec9285933a72b0ac8339f58
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2026-08-19 13:56:07 +0200

    minor fix

--- src/Bot/BotDefinition.php
+++ src/Bot/BotDefinition.php
@@ -19,7 +19,7 @@ readonly class BotDefinition
 		public ?string $robots_txt_token = null,
 		public string $description = '',
 		public string $info_url = '',
-		public string $ip_ranges_feed_url = [],
+		public array $ip_ranges_feed_url = [],
 		public BotAction $default_action = BotAction::ALLOW,
 		) {}
 
